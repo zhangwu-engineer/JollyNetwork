c92e68d5a81918bb79ba2bbf3f9ca44220c6e7e0
lalit-acuments <lalit@acuments.com>
2019-06-26 09:44:41 +0530

    Fixes cron job time to 10 minuts

--- src/services/CronJob.js
+++ src/services/CronJob.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const marketingEmails = require('../lib/marketingEmails');
 
 const monthlyDigestEmailCron = () => {
-  cron.schedule("*/1 * * * *", function() {
+  cron.schedule("*/10 * * * *", function() {
     console.log('cron job running');
     const marketing = new marketingEmails();
     marketing.monthlyDigestMailer();
