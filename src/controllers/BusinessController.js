c308fa0a96cad36815e7c0604203d77ab225679d
Zhang Wu <zhangwu.engineer@gmail.com>
2019-06-27 00:38:30 -0400

    find city businesses
    find city businesses

--- src/controllers/BusinessController.js
+++ src/controllers/BusinessController.js
@@ -2,7 +2,7 @@
  * Business controller class, in charge of transactions related to businesses.
  */
 const mongodb = require('mongodb');
-
+const Promise = require('bluebird');
 const EntityBusiness = require('../entities/EntityBusiness'),
 	DbNames = require('../enum/DbNames');
 
@@ -53,6 +53,20 @@ class BusinessController {
       throw err;
     }
   }
+
+  async getBusinessById(businessId) {
+    let business = null;
+    try {
+			business = await this.findBusinessById(businessId);
+      if (business) {
+        const businessData = business.toJson({});
+        return businessData;
+      }
+      throw new ApiError('Business not found');
+    } catch (err) {
+      throw err;
+    }
+  }
   
   findBusinessBySlug (slug) {
 
@@ -74,6 +88,77 @@ class BusinessController {
 			}).catch(reject);
 
 		});
+	}
+
+	findBusinessById (businessId) {
+
+		let db = this.getDefaultDB(),
+			business = null;
+			
+		return new Promise((resolve, reject) => {
+
+			db.collection('businesses').findOne({
+			  _id: new mongodb.ObjectID(businessId),
+			}).then((data) => {
+				if (data) {
+
+					business = new EntityBusiness(data);
+				}
+
+				resolve (business);
+
+			}).catch(reject);
+
+		});
+	}
+	
+	async searchCityBusinesses(city, query, page, perPage, role, activeStatus, userId) {
+    const db = this.getDefaultDB();
+    const skip = page && perPage ? (page - 1) * perPage : 0;
+    const aggregates = [
+      {
+        $match : {
+          userId: { $ne: new mongodb.ObjectID(userId) },
+        }
+      },
+      { $sort  : { userId : -1 } },
+    ];
+    if (query) {
+      aggregates.push({
+        $match : {
+          'slug': { $regex: new RegExp(`^${query.split(' ').join('-')}`, "i") },
+        }
+      });
+    }
+    if (page && perPage) {
+      aggregates.push({
+        $facet : {
+          meta: [ { $count: "total" }, { $addFields: { page: parseInt(page, 10) } } ],
+          data: [ { $skip:  skip}, { $limit:  perPage } ]
+        }
+      })
+    } else {
+      aggregates.push({
+        $facet : {
+          meta: [ { $count: "total" }, { $addFields: { page: parseInt(page, 10) } } ],
+          data: [ { $skip:  skip} ]
+        }
+      })
+    }
+    try {
+      const data = await db.collection('businesses').aggregate(aggregates).toArray();
+
+			const businessProfiles = data[0].data;
+      let businesses = await Promise.map(businessProfiles, profile => this.getBusinessById(profile._id));
+
+      return {
+        total: data[0].meta[0] ? data[0].meta[0].total : 0,
+        page: data[0].meta[0] && data[0].meta[0].page ? data[0].meta[0].page : 1,
+        businesses,
+      };
+    } catch (err) {
+      throw new ApiError(err.message);
+    }
   }
 	
 }
