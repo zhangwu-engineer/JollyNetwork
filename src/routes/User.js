49353891d33b175923bcefd445f589c881aab294
Zhang Wu <zhangwu.engineer@gmail.com>
2019-07-04 22:57:26 -0400

    remove already connected users to business
    remove already connected users to business

--- src/routes/User.js
+++ src/routes/User.js
@@ -176,7 +176,7 @@ router.get('/files', authService.verifyUserAuthentication, (req, res, next) => {
 });
 
 router.post('/city', authService.verifyUserAuthentication, (req, res, next) => {
-  userController.searchCityUsers(req.body.city, req.body.query, req.body.page, req.body.perPage, req.body.role, req.body.activeStatus, req.userId)
+  userController.searchCityUsers(req.body.city, req.body.query, req.body.page, req.body.perPage, req.body.role, req.body.activeStatus, req.body.businessId, req.userId)
     .then(data => {
       res.apiSuccess(data);
     })
