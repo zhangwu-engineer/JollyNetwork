210b5a2d1734a1e679c329f2da45ff71b3a12fbe
lalit-acuments <lalit@acuments.com>
2019-07-12 19:25:02 +0530

    added trusted column to users collection and return city and connection object from user api added json2csv package and authenticate admin via query params to export csv.

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -2,6 +2,8 @@
  * User Route Handler
  */
 const router = require('express').Router();
+const json2csv = require('json2csv').parse;
+// var stringify = require('csv-stringify');
 const jwt = require('jsonwebtoken');
 const Promise = require('bluebird');
 const asyncMiddleware = require('../lib/AsyncMiddleware');
@@ -22,9 +24,30 @@ let authService = JOLLY.service.Authentication,
 router.post('/user', authService.verifyUserAuthentication, asyncMiddleware(async (req, res, next) => {
 
   const result = await userController.searchUsers(req.body);
-
   res.apiSuccess(result);
 
+}));
+
+router.get('/user/trusted/:userId', authService.verifyUserAuthentication, asyncMiddleware(async (req, res, next) => {
+  userController.setUserTrusted(req.params.userId)
+    .then(userData => {
+      res.apiSuccess(userData);
+    }).catch(next);
+}));
+
+router.get('/users/csv', authService.verifyUserAuthentication, asyncMiddleware(async (req, res, next) => {
+  try {
+    const users = await userController.searchUsers({});
+    const fields = ['email', 'firstName', 'lastName', 'city', 'connections','trusted', 'jobs',
+      'topPosition','top2ndPosition','posts','coworkers','date_created'
+    ];
+    const csvData = json2csv(users.data, {fields});
+    res.set('Content-Type', 'text/csv');
+    res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'users-' + Date.now() + '.csv\"');
+    res.send(csvData)
+  } catch (err) {
+    throw err
+  }
 }));
 
 module.exports = router;
