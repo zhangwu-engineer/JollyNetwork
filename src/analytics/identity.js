536995864c257867a85384a57ec85c0f833febc6
Ronak Gothi <ronakjain90@gmail.com>
2019-07-17 15:01:00 +0530

    Segment: count post marked as helpful

--- src/analytics/identity.js
+++ src/analytics/identity.js
@@ -17,9 +17,24 @@ class IdentityAnalytics {
     const countPostAdded = postController.getUserPostCount(user.id);
     const countCoworkerConnections = connectionController.getUserConnectionsCount(user.id, {isCoworker: true, connectionType: ['f2f']});
     const countGenericConnections = connectionController.getUserConnectionsCount(user.id, {isCoworker: false, connectionType: ['f2f']});
+    const countPostHelpful = postController.getUserPostHelpfulCount(user.id);
 
-    Promise.all([positionsAdded, countJobsAdded, countPostAdded, countCoworkerConnections, countGenericConnections]).then((result) => {
-      console.log(result);
+    Promise.all([positionsAdded, countJobsAdded, countPostAdded, countCoworkerConnections, countGenericConnections, countPostHelpful]).then((result) => {
+      console.log(userId, result);
+      let params = {
+        all_positions: result[0],
+        count_positions_added: result[0].length,
+        count_jobs_added: result[1],
+        count_feed_posts: result[2],
+        count_f2f_coworker_connections: result[3],
+        count_f2f_generic_connections: result[4],
+        count_posts_marked_heplful: result[5]
+      };
+      if (user.email.includes('@jollyhq.com')) params.test = true;
+      this.analytics.identify({
+        userId: userId.toString(),
+        traits: params,
+      });
     })
   }
 }
