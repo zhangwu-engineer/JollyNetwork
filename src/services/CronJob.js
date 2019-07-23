47ba746586fce97b568cb196b1d9c1753d2b3686
Ronak Gothi <ronakjain90@gmail.com>
2019-07-23 19:05:44 +0530

    Fix

--- src/services/CronJob.js
+++ src/services/CronJob.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const marketingEmails = require('../lib/marketingEmails');
 
 const coworkersConnectingMailCron = () => {
-  cron.schedule("31 13 23 * *", function() {
+  cron.schedule("40 13 23 * *", function() {
     console.log('cron job running');
     const marketing = new marketingEmails();
     marketing.coworkersConnectingMailer();
