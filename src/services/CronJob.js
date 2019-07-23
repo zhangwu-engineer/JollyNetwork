0f5922b317d8024d2525f153188298bb6e3ad323
Zhang Wu <zhangwu.engineer@gmail.com>
2019-07-23 17:22:41 +0530

    Updated CRON

--- src/services/CronJob.js
+++ src/services/CronJob.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const marketingEmails = require('../lib/marketingEmails');
 
 const monthlyDigestMailCron = () => {
-  cron.schedule("50 11 23 * *", function() {
+  cron.schedule("00 12 23 * *", function() {
     console.log('cron job running');
     const marketing = new marketingEmails();
     marketing.monthlyDigestMailer();
