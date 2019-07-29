27da5790424b3b9cbebc6d19795a120beb0c8acb
Zhang Wu <zhangwu.engineer@gmail.com>
2019-07-29 12:50:48 +0530

    Final monthly mailer fixes

--- src/services/CronJob.js
+++ src/services/CronJob.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const marketingEmails = require('../lib/marketingEmails');
 
 const monthlyDigestMailCron = () => {
-  cron.schedule("35 12 23 * *", function() {
+  cron.schedule("30 7 1 * *", function() {
     console.log('cron job running');
     const marketing = new marketingEmails();
     marketing.monthlyDigestMailer();
